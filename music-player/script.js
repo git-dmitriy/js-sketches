91350eb33e5dea23966eff41b71db749933fe7e1
Dmitriy <shalberkind@mail.ru>
2021-07-15 23:29:21 +0300

    Create prev - next functionality

--- music-player/script.js
+++ music-player/script.js
@@ -3,7 +3,35 @@ const playBtn = document.querySelector('#play');
 const prevBtn = document.querySelector('#prev');
 const nextBtn = document.querySelector('#next');
 
+const image = document.querySelector('#img');
+const title = document.querySelector('#title');
+const artist = document.querySelector('#artist');
+
 let isPlaying = false;
+let currentSong = 0;
+
+const songs = [
+  {
+    name: 'jacinto-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'jacinto-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'jacinto-3',
+    displayName: 'Goodnight Disco Queen',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Metric/Jacinto Design',
+  },
+];
 
 const playSong = () => {
   isPlaying = true;
@@ -18,4 +46,27 @@ const pauseSong = () => {
   playBtn.setAttribute('title', 'Play');
 };
 
+const loadSong = (songs) => {
+  title.textContent = songs.displayName;
+  artist.textContent = songs.artist;
+  music.src = `music/${songs.name}.mp3`;
+  image.src = `img/${songs.name}.jpg`;
+};
+
+loadSong(songs[currentSong]);
+
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+prevBtn.addEventListener('click', () => {
+  currentSong--;
+  if (currentSong < 0) currentSong = songs.length - 1;
+  loadSong(songs[currentSong]);
+  playSong();
+});
+
+nextBtn.addEventListener('click', () => {
+  currentSong++;
+  if (currentSong > songs.length - 1) currentSong = 0;
+  loadSong(songs[currentSong]);
+  playSong();
+});
