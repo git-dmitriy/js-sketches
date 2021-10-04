b2adbc7c3c3e7ba55eff5edd1862ea721ed1a468
git-dmitriy <shalberkind@mail.ru>
2021-10-04 23:58:08 +0300

    Extending tailwind animation

--- blurry-animated-background/tailwind.config.js
+++ blurry-animated-background/tailwind.config.js
@@ -2,10 +2,30 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blob: 'blob 10s infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%': {
+            transform: 'scale(1)',
+          },
+          '33%': {
+            transform: 'scale(1.2)',
+          },
+          '66%': {
+            transform: 'scale(0.8)',
+          },
+          '100%': {
+            transform: 'scale(1)',
+          },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
